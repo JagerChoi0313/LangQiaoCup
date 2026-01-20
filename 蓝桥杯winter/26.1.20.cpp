8503bfe47ed12939f98b8ee5ca1dda5bd0a2d2e5
JagerChoi <17322905680@qq.com>
2026-01-20 22:21:51 +0800

    Update: 2026.1.20再次修改蓝桥杯练习代码26.1.20.cpp

--- 蓝桥杯winter/26.1.20.cpp
+++ 蓝桥杯winter/26.1.20.cpp
@@ -178,50 +178,72 @@
 //}
 
 
-#include <iostream>
-#include <sstream>
-#include <vector>
-#include <string>
 
-using namespace std;
 
-int main()
-{
-	string s;
-	getline(cin, s);
 
-	if (!s.empty() && s.back() == '.')
-	{
-		s.pop_back();
-	}
 
-	stringstream ss(s);
-	vector<string>arr;
-	string word;
 
-	while (ss >>word )
-	{
-		arr.push_back(word);
-	}
+//最长单词
+//一个以.结尾的简单英文句子，单词之间用单个空格分隔，没有缩写形式和其它特殊形式，求句子中的最长单词。
+//
+//输入格式
+//输入一行字符串，表示这个简单英文句子，长度不超过 500
+//。
+//
+//输出格式
+//该句子中最长的单词。如果多于一个，则输出第一个。
+//
+//输入样例：
+//I am a student of Peking University.
+//输出样例：
+//University
 
-	int Maxlen = 0;
-	string longestword;
-	
-	for (int i = 0; i < arr.size(); i++)
-	{
-		string w = arr[i];
-		int len = w.length();//使用string的length方法
-		if (len > Maxlen)
-		{
-			Maxlen = len;
-			longestword = w;
-		}
-	}
 
-	cout << longestword;
 
-	return 0;
-}
+//#include <iostream>
+//#include <sstream>
+//#include <vector>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string s;
+//	getline(cin, s);
+//
+//	if (!s.empty() && s.back() == '.')
+//	{
+//		s.pop_back();
+//	}
+//
+//	stringstream ss(s);
+//	vector<string>arr;
+//	string word;
+//
+//	while (ss >>word )
+//	{
+//		arr.push_back(word);
+//	}
+//
+//	int Maxlen = 0;
+//	string longestword;
+//	
+//	for (int i = 0; i < arr.size(); i++)
+//	{
+//		string w = arr[i];
+//		int len = w.length();//使用string的length方法
+//		if (len > Maxlen)
+//		{
+//			Maxlen = len;
+//			longestword = w;
+//		}
+//	}
+//
+//	cout << longestword;
+//
+//	return 0;
+//}
 
 
 
