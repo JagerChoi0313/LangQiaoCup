9b91e8adaea47e88a7a737b9733bf07f6f0175bb
JagerChoi <17322905680@qq.com>
2026-01-30 22:54:58 +0800

    Update: 2026.1.30修改蓝桥杯练习代码26.1.30.cpp，新增从零开始学编程的步骤.md文件

--- 蓝桥杯winter/26.1.30.cpp
+++ 蓝桥杯winter/26.1.30.cpp
@@ -67,3 +67,100 @@
 //
 //    return 0;
 //}
+
+
+//最大化股票交易的利润
+
+//#include <iostream>
+//#include <vector>
+//#include <climits>
+//using namespace std;
+//int main()
+//{
+//    // 请在此输入您的代码
+//    int N;
+//    cin >> N;
+//
+//    vector<int>prices(N);
+//    for (int i = 0; i < N; i++)
+//    {
+//        cin >> prices[i];
+//    }
+//
+//    //记录历史最低买入价
+//    int minprice = prices[0];
+//
+//    //记录最大利润
+//    int maxprofit = prices[1] - prices[0];
+//
+//    for (int i = 1; i < N; i++)
+//    {
+//        //如果今天卖出能得到的利润
+//        int profit = prices[i] - minprice;
+//
+//        //更新最大利润
+//        if (profit > maxprofit)
+//        {
+//            maxprofit = profit;
+//        }
+//
+//        //更新历史最低价（为以后买入做准备）
+//        if (prices[i] < minprice)
+//        {
+//            minprice = prices[i];
+//        }
+//
+//    }
+//
+//    cout << maxprofit;
+//
+//    return 0;
+//}
+
+
+//方法二
+//#include <iostream>
+//#include <vector>
+//#include <climits>
+//using namespace std;
+//int main()
+//{
+//    // 请在此输入您的代码
+//    int N;
+//    cin >> N;
+//
+//    vector<int>prices(N);
+//    for (int i = 0; i < N; i++)
+//    {
+//        cin >> prices[i];
+//    }
+//
+//    //dp[i]:第i天卖出时的最大利润
+//    vector<int>dp(N);
+//
+//    //minprice(N):  前i天的最低价格
+//    vector<int>minprice(N);
+//
+//    //初始化
+//    minprice[0] = prices[0];
+//    dp[0] = 0;//第0天无法卖出，记为0
+//
+//    int ans = INT_MIN;
+//
+//    for (int i = 1; i < N; i++)
+//    {
+//        minprice[i] = min(minprice[i - 1], prices[i]);
+//
+//        dp[i] = prices[i] - minprice[i - 1];//为什么不是minprice[i]，因为minprice[i]可能是当天的价格，那就变成了当天买当天卖
+//
+//        //更新答案
+//        ans = max(ans, dp[i]);
+//
+//    }
+//
+//    cout << ans << endl;
+//
+//
+//    return 0;
+//}
+
