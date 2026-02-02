c10c00b10b9bfa13b461ed7d4c02b2bd0d29f669
JagerChoi <17322905680@qq.com>
2026-02-03 00:11:59 +0800

    Update: 2026.2.3修改蓝桥杯练习代码，新增字符数组.md、26.2.2.cpp文件

---
+++ 蓝桥杯winter/26.2.2.cpp
@@ -0,0 +1,25 @@
+//Ã®Ω◊Œ Ã‚
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<long long> dp(N + 1, 0);
+    dp[0] = 1;
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int step = 1; step <= i; step++)
+        {
+            dp[i] += dp[i - step];
+        }
+    }
+
+    cout << dp[N];
+    return 0;
+}
